558adcafced6f3b68abac13841f5a8aab5c7f011
Nathan Ostrander <ostrander_nathan@yahoo.com>
2017-06-12 13:48:28 -0500

    complete

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,6 +1,8 @@
 "use strict";
 app.controller("HomeController", function($scope, $location) {
 
-
+ $scope.goToAbout = () => {
+        $location.url('/about');
+    };
 
 });
--- js/controllers/PortfolioController.js
+++ js/controllers/PortfolioController.js
@@ -1,8 +1,5 @@
 "use strict";
 app.controller("PortfolioController", function($scope, $location) {
 
-    $('#myCarousel').carousel({
-        interval: 10000
-    })
 
 });
